1e985f089a5932ce93d9006f0f769bd6225444d4
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-02-27 18:58:55 -0800

    tested if fetchCoordsByIP is imported successfully

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP } = require("./iss");
+const { fetchMyIP, fetchCoordsByIP } = require("./iss");
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -7,4 +7,8 @@ const { fetchMyIP } = require("./iss");
 //   }
 
 //   console.log("It worked! Returned IP:", ip);
+// });
+
+// fetchCoordsByIP("24.80.178.224", (error, data) => {
+//   console.log(error, data);
 // });
