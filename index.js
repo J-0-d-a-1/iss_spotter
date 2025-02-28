99091c503da66162ea58e4e87f3a8077e77ea4c5
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-02-27 20:12:25 -0800

    tested if fetchISSFlyOverTimes is imported successfully

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP } = require("./iss");
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require("./iss");
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -9,6 +9,23 @@ const { fetchMyIP, fetchCoordsByIP } = require("./iss");
 //   console.log("It worked! Returned IP:", ip);
 // });
 
-// fetchCoordsByIP("24.80.178.224", (error, data) => {
-//   console.log(error, data);
+// fetchCoordsByIP("24.80.178.224", (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log("It worked! Returned coordinates:", coordinates);
 // });
+
+// fetchISSFlyOverTimes(
+//   { latitude: "49.27670", longitude: "-123.13000" },
+//   (error, coordinates) => {
+//     if (error) {
+//       console.log("It didn't work!", error);
+//       return;
+//     }
+
+//     console.log("It worked! Returned coordinates:", coordinates);
+//   }
+// );
